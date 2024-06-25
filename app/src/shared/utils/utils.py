8b35b6064615eaf0192535f18c480fe998424658
Nathan Souza <N4thansouz4@gmail.com>
2024-06-24 23:02:20 -0300

    deleting old files

--- app/src/shared/utils/utils.py
+++ app/src/shared/utils/utils.py
@@ -1,9 +1,9 @@
-import streamlit as st
+# import streamlit as st
 
 
-# def clear_chat_history():
-#     """
-#     Clears the chat history stored in the Streamlit session state.
-#     """
-#     st.session_state.messages = []
-#     return
+# # def clear_chat_history():
+# #     """
+# #     Clears the chat history stored in the Streamlit session state.
+# #     """
+# #     st.session_state.messages = []
+# #     return
